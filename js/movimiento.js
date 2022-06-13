43a8672feeb824c71cfe006691370ca777ee8791
joadioz <joaquin.pk.jidp@gmail.com>
2022-06-13 19:09:35 -0400

    cambios gigantes, vendedor listo

--- js/movimiento.js
+++ js/movimiento.js
@@ -1,15 +1,38 @@
 const contenedor = document.getElementById("cambiante");
-const cardSteam = document.getElementById("steam-bot");
+const steamBotButton = document.getElementById("steam-bot-button");
+const buttonTrabajo = document.getElementById("button-trabajo");
+const steamBot = document.getElementById("steam-bot");
+contenedorOriginal = contenedor.innerHTML;
+const eliminarInfo = document.getElementById("eliminar-info");
+const parallax1 = document.getElementById("parallax-1");
 
 // Funcion que cambia el contenido del contenedor
-const contenido = "hola";
-function cambiarContenido(contenido) {
-	contenidoViejo = contenedor.innerHTML; 
-	contenedor.innerHTML = contenido;
-}
+
 // funcion que se activa al dar click
-function cambiarContenidoClick() {
-	cambiarContenido(contenido);
+//esconder contenedor
+function esconderContenedor() {
+	if (contenedor.classList.contains("is-hidden")) {
+		contenedor.classList.remove("is-hidden");
+		contenedor.classList.remove("desaparecer");
+		contenedor.classList.add("aparecer");
+		steamBot.className += " is-hidden";
+	}
 }
-//darle una función a un contenedor
-cardSteam.addEventListener("click", cambiarContenidoClick);
+function mostrarSteam() {
+	contenedor.classList.add("desaparecer");
+	setTimeout(function () {
+		contenedor.className += " is-hidden";
+		steamBot.classList.remove("is-hidden");
+	}, 500);
+}
+function ocultarInfo() {
+	if (parallax1.classList.contains("animacion-arriba")) {
+		parallax1.classList.remove("animacion-arriba");
+	} else {
+		parallax1.classList.add("animacion-arriba");
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+	}
+}
+buttonTrabajo.addEventListener("click", esconderContenedor);
+steamBotButton.addEventListener("click", mostrarSteam);
+eliminarInfo.addEventListener("click", ocultarInfo);
